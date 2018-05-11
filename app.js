bf9e159b64586beb0703b2f1095b2cd82d503c76
JerryMissTom <582486638@qq.com>
2018-05-11 16:39:37 +0800

    模板引擎

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const Koa = require('koa')
 const path = require('path')
 const bodyParser = require('koa-bodyparser')
+const nunjucks = require('koa-nunjucks-2')
+
 const app = new Koa()
 const router=require('./router')
 
 app.use(bodyParser())
+
+app.use(nunjucks({
+    ext: 'html',
+    path: path.join(__dirname, 'views'),// 指定视图目录
+    nunjucksConfig: {
+      trimBlocks: true // 开启转义 防Xss
+    }
+  }))
 
 router(app)
 
