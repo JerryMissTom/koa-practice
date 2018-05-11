2e7259d686503d45b41fd2d1da97f0fe822770b9
JerryMissTom <582486638@qq.com>
2018-05-11 13:50:35 +0800

    路由的内容

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
-// 按照官方示例
 const Koa = require('koa')
+// 注意 require('koa-router') 返回的是函数:
+const Router = require('koa-router')
 const app = new Koa()
-
-// 记录执行的时间
-app.use(async (ctx, next) => {
-  let stime = new Date().getTime()
-  await next()
-  let etime = new Date().getTime()
-  ctx.response.type = 'text/html'
-  ctx.response.body = '<h1>Hello World</h1>'
-  console.log(`请求地址: ${ctx.path}，响应时间：${etime - stime}ms`)
-});
-
-app.use(async (ctx, next) => {
-  console.log('中间件1 doSoming')
-  await next();
-  console.log('中间件1 end')
+const router=new Router();
+ // 添加路由
+ router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>index page</h1>`
 })
 
-app.use(async (ctx, next) => {
-  console.log('中间件2 doSoming')
-  await next();
-  console.log('中间件2 end')
+router.get('/home', async (ctx, next) => {
+    ctx.response.body = '<h1>HOME page</h1>'
 })
 
-app.use(async (ctx, next) => {
-  console.log('中间件3 doSoming')
-  await next();
-  console.log('中间件3 end')
+router.get('/404', async (ctx, next) => {
+    ctx.response.body = '<h1>404 Not Found</h1>'
 })
 
-app.listen(3000, () => {
+ // 调用路由中间件
+ app.use(router.routes())
+
+app.listen(3000, ()=>{
   console.log('server is running at http://localhost:3000')
 })
