2f068db44dfc8cdf5af3ff2f27567ed9bf37e71f
JerryMissTom <582486638@qq.com>
2018-05-14 09:46:09 +0800

    JSON数据处理

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const Koa = require('koa')
-const path = require('path')
-const bodyParser = require('koa-bodyparser')
-const nunjucks = require('koa-nunjucks-2')
-const staticFiles = require('koa-static')
-
 const app = new Koa()
-const router=require('./router')
-app.use(staticFiles(path.resolve(__dirname, "./public")))
+const router = require('./router')
+const middlewre = require('./middleware')
 
-app.use(nunjucks({
-    ext: 'html',
-    path: path.join(__dirname, 'views'),// 指定视图目录
-    nunjucksConfig: {
-      trimBlocks: true // 开启转义 防Xss
-    }
-  }))
-
-  app.use(bodyParser())
+middlewre(app)
 router(app)
 
 app.listen(3000, () => {
-    console.log('server is running at http://localhost:3000')
+  console.log('server is running at http://localhost:3000')
 })
