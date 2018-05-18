9261e622bc8db5e4f692a9a4171e1f50644cac97
JerryMissTom <582486638@qq.com>
2018-05-18 13:31:25 +0800

    错误界面处理

--- middleware/index.js
+++ middleware/index.js
@@ -1,20 +1,24 @@
 const path = require('path')
-const ip = require('ip')
+const ip = require("ip")
 const bodyParser = require('koa-bodyparser')
 const nunjucks = require('koa-nunjucks-2')
 const staticFiles = require('koa-static')
 
 const miSend = require('./mi-send')
 const miLog = require('./mi-log')
+const miHttpError = require('./mi-http-error')
 module.exports = (app) => {
+  app.use(miHttpError({
+    errorPageFolder: path.resolve(__dirname, '../errorPage')
+  }))
 
-  app.use(miLog({
-    env: app.env, // koa 提供的环境变量
+  app.use(miLog(app.env, {
+    env: app.env,
     projectName: 'koa2-tutorial',
     appLogLevel: 'debug',
     dir: 'logs',
     serverIp: ip.address()
-  }))
+  }));
 
   app.use(staticFiles(path.resolve(__dirname, "../public")))
 
@@ -28,4 +32,16 @@ module.exports = (app) => {
 
   app.use(bodyParser())
   app.use(miSend())
+
+  // 增加错误的监听处理
+  app.on("error", (err, ctx) => {
+    if (ctx && !ctx.headerSent && ctx.status < 500) {
+      ctx.status = 500
+    }
+    if (ctx && ctx.log && ctx.log.error) {
+      if (!ctx.state.logged) {
+        ctx.log.error(err.stack)
+      }
+    }
+  }) 
 }
